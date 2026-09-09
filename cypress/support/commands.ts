cbf2a66972ce0537437bf0635113bbc7a25e6b28
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2026-09-09 21:56:27 +0700

    test(e2e): cleanup deprecated custom commands and old screenshots

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,16 +1,1 @@
 // Custom Cypress commands can be added here
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>
-    }
-  }
-}
-
-Cypress.Commands.add('login', (email: string, password: string) => {
-  cy.visit('/login')
-  cy.get('input[type="email"]').type(email)
-  cy.get('input[type="password"]').type(password)
-  cy.get('button[type="submit"]').click()
-  cy.url().should('include', '/dashboard')
-})
